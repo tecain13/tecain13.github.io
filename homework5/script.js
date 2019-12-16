734a26cad61f97ca68baa6c7584f69a140ea4b7f
Tyler Cain <53357823+tecain13@users.noreply.github.com>
2019-12-16 13:33:11 -0700

    Add files via upload

---
+++ homework5/script.js
@@ -0,0 +1,21 @@
+var currenthour //turn red
+var pasthour //turn grey
+var futurehour // turn green
+var time;
+
+
+//save function w/ local storage retention
+
+//recognize current time
+
+//compare current time to other functions
+
+//project future hours green
+//if time > current hour, turn green?
+
+//project current hour red
+//if time = current hour, turn red
+
+//project previous hours grey
+//if pasthour < current houe, turn grey
+
